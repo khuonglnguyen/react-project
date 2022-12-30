ffba3bf2e06696144c995480cae1b6a13c81cc1f
victornguyen0412 <victor.nguyen@beyondedge.co>
2022-12-30 15:21:40 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Button, Layout } from "antd";
+import { Content, Footer, Header } from "antd/es/layout/layout";
+
+function App() {
+  return (
+    <Layout>
+      <Header>Header</Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Layout>
+  );
+}
+
+export default App;
